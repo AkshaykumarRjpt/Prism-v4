93e4c0550a9f13c747524361ca0a2871ec8909f2
AkshaykumarRjpt <akshay.kumar.rjpt@gmail.com>
2018-12-16 15:29:32 +0530

    Added A dummy Module and created basic region(itemcontrol and content control)

---
+++ ModuleA/ModuleAModule.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using Microsoft.Practices.Unity;
+using PrismDemo.Infrastucture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleA
+{
+    public class ModuleAModule : IModule
+    {
+        IUnityContainer _container;
+        IRegionManager _regionManger;
+
+        public ModuleAModule(IUnityContainer container, IRegionManager regionManger)
+        {
+            this._container = container;
+            this._regionManger = regionManger;
+        }
+
+        public void Initialize()
+        {
+            IRegion region = _regionManger.Regions[RegionNames.ToolBarRegion];
+            region.Add(_container.Resolve<TabViewControl>());
+            region.Add(_container.Resolve<TabViewControl>());
+            region.Add(_container.Resolve<TabViewControl>());
+            region.Add(_container.Resolve<TabViewControl>());
+            region.Add(_container.Resolve<TabViewControl>());
+            //_regionManger.RegisterViewWithRegion(RegionNames.ToolBarRegion,typeof(TabViewControl));
+            _regionManger.RegisterViewWithRegion(RegionNames.ContentRegion, typeof(ContentView));
+        }
+    }
+}
--- src/PrismDemo/Bootstrapper.cs
+++ src/PrismDemo/Bootstrapper.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.Practices.Prism.UnityExtensions;
 using Microsoft.Practices.Unity;
+using Microsoft.Practices.Prism;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Practices.Prism.Modularity;
+using ModuleA;
 
 namespace PrismDemo
 {
@@ -23,5 +26,19 @@ namespace PrismDemo
             App.Current.MainWindow = (Window)Shell;
             App.Current.MainWindow.Show();
         }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            Type moduletype = typeof(ModuleAModule);
+            ModuleCatalog.AddModule(new ModuleInfo
+            {
+                ModuleName=moduletype.Name,
+                ModuleType=moduletype.AssemblyQualifiedName,
+
+                InitializationMode= InitializationMode.WhenAvailable
+            });
+        }
+
+       
     }
 }
