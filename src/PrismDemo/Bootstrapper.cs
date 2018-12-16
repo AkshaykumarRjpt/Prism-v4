81ce986e2907db43777e2a31b584d13599feb369
AkshaykumarRjpt <akshay.kumar.rjpt@gmail.com>
2018-12-16 12:56:45 +0530

    Added Bootstrapper(not implemented)

---
+++ src/PrismDemo/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismDemo
+{
+    public class Bootstrapper  : UnityBootstrapper
+    {
+        protected override System.Windows.DependencyObject CreateShell()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
