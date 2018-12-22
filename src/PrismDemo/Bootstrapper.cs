c04def1203c11b419e594fa345bb25cd687bfc7b
AkshaykumarRjpt <akshay.kumar.rjpt@gmail.com>
2018-12-22 15:55:15 +0530

    Loading Module From Directory And Xaml

--- src/PrismDemo/Bootstrapper.cs
+++ src/PrismDemo/Bootstrapper.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using Microsoft.Practices.Prism.Modularity;
-using ModuleA;
+
 using Microsoft.Practices.Prism.Regions;
 using System.Windows.Controls;
 using PrismDemo.Infrastucture;
@@ -30,17 +30,30 @@ namespace PrismDemo
             App.Current.MainWindow.Show();
         }
 
-        protected override void ConfigureModuleCatalog()
-        {
-            Type moduletype = typeof(ModuleAModule);
-            ModuleCatalog.AddModule(new ModuleInfo
-            {
-                ModuleName=moduletype.Name,
-                ModuleType=moduletype.AssemblyQualifiedName,
 
-                InitializationMode= InitializationMode.WhenAvailable
-            });
+        /* Loading module from the Directory use Directory module catalog*/
+        //protected override IModuleCatalog CreateModuleCatalog()
+        //{
+        //    string path = new Uri(Environment.CurrentDirectory+@"\Modules",UriKind.Relative).ToString();
+        //    return new DirectoryModuleCatalog() { ModulePath = @"./Modules" };
+        //}
+
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            return Microsoft.Practices.Prism.Modularity.ModuleCatalog.CreateFromXaml(new Uri("/PrismDemo;component/XamlCatalog.xaml", UriKind.Relative));
         }
+
+        //protected override void ConfigureModuleCatalog()
+        //{
+        //    Type moduletype = typeof(ModuleAModule);
+        //    ModuleCatalog.AddModule(new ModuleInfo
+        //    {
+        //        ModuleName=moduletype.Name,
+        //        ModuleType=moduletype.AssemblyQualifiedName,
+
+        //        InitializationMode= InitializationMode.WhenAvailable
+        //    });
+        //}
 
         protected override RegionAdapterMappings ConfigureRegionAdapterMappings()
         {
